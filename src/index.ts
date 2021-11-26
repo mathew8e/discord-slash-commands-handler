983ef9c6e8836f53bc05b7d1710dc21d86752eee
Mathew Bosak <51512685+mathew8e@users.noreply.github.com>
2021-11-26 16:04:18 +0100

    get rekt

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { readdirSync } from 'fs';
 const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES']});
 
 readdirSync('./src/events').forEach(async (file) => {
+    // sean stinks
     if (!file.endsWith('.ts') && !file.endsWith('.js')) return;
     const eventName = file.split('.')[0];
     const event = (await import(`./events/${eventName}`)).default;
